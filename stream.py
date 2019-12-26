99675ccac0e5641177f664c7e3944f77565a1d2d
Ritheesh <bunnyrb4@gmail.com>
2019-12-26 18:19:01 +0530

    added app

---
+++ stream.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+from werkzeug.wrappers import Request, Response
+from flask import Flask, render_template, Response
+import cv2
+import sys
+import numpy
+# YOLO v3
+import os
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+import struct
+import cv2
+from numpy import expand_dims
+import tensorflow as tf
+from skimage.transform import resize
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D, BatchNormalization, LeakyReLU, ZeroPadding2D, UpSampling2D
+from keras.models import load_model, Model
+from keras.layers.merge import add, concatenate
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from matplotlib.patches import Rectangle
+
+
+
+#%matplotlib inline
+
+
+# load yolov3 model
+yolov3 = load_model('yolov3.h5')
+
+from numpy import expand_dims
+def load_image_pixels(filename, shape):
+    # load the image to get its shape
+    image = load_img(filename)
+    width, height = image.size
+    # load the image with the required size
+    image = load_img(filename, target_size=shape)
+    # convert to numpy array
+    image = img_to_array(image)
+    # scale pixel values to [0, 1]
+    image = image.astype('float32')
+    image /= 255.0
+    # add a dimension so that we have one sample
+    image = expand_dims(image, 0)
+    return image, width, height
+
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, objness = None, classes = None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        
+        self.objness = objness
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+        
+        return self.label
+    
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+            
+        return self.score
+
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def _interval_overlap(interval_a, interval_b):
+    x1, x2 = interval_a
+    x3, x4 = interval_b
+
+    if x3 < x1:
+        if x4 < x1:
+            return 0
+        else:
+            return min(x2,x4) - x1
+    else:
+        if x2 < x3:
+             return 0
+        else:
+            return min(x2,x4) - x3 
+def bbox_iou(box1, box2):
+    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
+    
+    intersect = intersect_w * intersect_h
+
+    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+    
+    union = w1*h1 + w2*h2 - intersect
+    
+    return float(intersect) / union
+
+def do_nms(boxes, nms_thresh):
+    if len(boxes) > 0:
+        nb_class = len(boxes[0].classes)
+    else:
+        return
+        
+    for c in range(nb_class):
+        sorted_indices = np.argsort([-box.classes[c] for box in boxes])
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+
+            if boxes[index_i].classes[c] == 0: continue
+
+            for j in range(i+1, len(sorted_indices)):
+                index_j = sorted_indices[j]
+
+                if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
+                    boxes[index_j].classes[c] = 0
+
+#decode_netout() that will take each one of the NumPy arrays, one at a time, 
+#and decode the candidate bounding boxes and class predictions
+def decode_netout(netout, anchors, obj_thresh,  net_h, net_w):
+    grid_h, grid_w = netout.shape[:2]
+    nb_box = 3
+    netout = netout.reshape((grid_h, grid_w, nb_box, -1))
+    nb_class = netout.shape[-1] - 5
+
+    boxes = []
+
+    netout[..., :2]  = _sigmoid(netout[..., :2])
+    netout[..., 4:]  = _sigmoid(netout[..., 4:])
+    netout[..., 5:]  = netout[..., 4][..., np.newaxis] * netout[..., 5:]
+    netout[..., 5:] *= netout[..., 5:] > obj_thresh
+
+    for i in range(grid_h*grid_w):
+        row = i / grid_w
+        col = i % grid_w
+        
+        for b in range(nb_box):
+            # 4th element is objectness score
+            objectness = netout[int(row)][int(col)][b][4]
+            #objectness = netout[..., :4]
+            
+            if(objectness.all() <= obj_thresh): continue
+            
+            # first 4 elements are x, y, w, and h
+            x, y, w, h = netout[int(row)][int(col)][b][:4]
+
+            x = (col + x) / grid_w # center position, unit: image width
+            y = (row + y) / grid_h # center position, unit: image height
+            w = anchors[2 * b + 0] * np.exp(w) / net_w # unit: image width
+            h = anchors[2 * b + 1] * np.exp(h) / net_h # unit: image height  
+            
+            # last elements are class probabilities
+            classes = netout[int(row)][col][b][5:]
+            
+            box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, objectness, classes)
+            #box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, None, classes)
+
+            boxes.append(box)
+
+    return boxes
+
+# bounding boxes will be stretched back into the shape of the original image
+#will allow plotting the original image and draw the bounding boxes, hopefully detecting real objects.
+# correct the sizes of the bounding boxes for the shape of the image
+#correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
+def correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w):
+    if (float(net_w)/image_w) < (float(net_h)/image_h):
+        new_w = net_w
+        new_h = (image_h*net_w)/image_w
+    else:
+        new_h = net_w
+        new_w = (image_w*net_h)/image_h
+        
+    for i in range(len(boxes)):
+        x_offset, x_scale = (net_w - new_w)/2./net_w, float(new_w)/net_w
+        y_offset, y_scale = (net_h - new_h)/2./net_h, float(new_h)/net_h
+        
+        boxes[i].xmin = int((boxes[i].xmin - x_offset) / x_scale * image_w)
+        boxes[i].xmax = int((boxes[i].xmax - x_offset) / x_scale * image_w)
+        boxes[i].ymin = int((boxes[i].ymin - y_offset) / y_scale * image_h)
+        boxes[i].ymax = int((boxes[i].ymax - y_offset) / y_scale * image_h)
+
+from matplotlib.patches import Rectangle
+def draw_boxes(filename, v_boxes, v_labels, v_scores):
+    # load the image
+    data = plt.imread(filename)
+    # plot the image
+    plt.imshow(data)
+    # get the context for drawing boxes
+    ax = plt.gca()
+    # plot each box
+    for i in range(len(v_boxes)):
+        box = v_boxes[i]
+        # get coordinates
+        y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
+        # calculate width and height of the box
+        width, height = x2 - x1, y2 - y1
+        # create the shape
+        rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+        # draw the box
+        ax.add_patch(rect)
+        # draw text and score in top left corner
+        label = "%s (%.3f)" % (v_labels[i], v_scores[i])
+        plt.text(x1, y1, label, color='red')
+    # show the plot
+    plt.show()
+
+# get all of the results above a threshold
+# takes the list of boxes, known labels, 
+#and our classification threshold as arguments and returns parallel lists of boxes, labels, and scores.
+def get_boxes(boxes, labels, thresh):
+    v_boxes, v_labels, v_scores = list(), list(), list()
+    # enumerate all boxes
+    for box in boxes:
+        # enumerate all possible labels
+        for i in range(len(labels)):
+            # check if the threshold for this label is high enough
+            if box.classes[i] > thresh:
+                v_boxes.append(box)
+                v_labels.append(labels[i])
+                v_scores.append(box.classes[i]*100)
+                # don't break, many labels may trigger for one box
+    return v_boxes, v_labels, v_scores
+
+input_w, input_h = 416, 416
+net_h,net_w=416,416
+obj_thresh, nms_thresh = 0.5, 0.45
+anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+class_threshold = 0.6
+labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
+              "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", \
+              "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", \
+              "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", \
+              "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", \
+              "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", \
+              "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", \
+              "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
+              "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
+              "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def get_frame():
+    camera_port=0
+    cap=cv2.VideoCapture(camera_port) #this makes a web cam object
+        
+    cap=cv2.VideoCapture(0)
+    while True:
+        ret,test_img=cap.read()# captures frame and returns boolean value and captured image
+        # define our new photo
+        im_name='YOLO'+'.jpg'
+        cv2.imwrite(im_name,test_img)
+        if not ret:
+            continue
+        # load and prepare image
+        image, image_w, image_h = load_image_pixels(im_name, (net_w, net_w))
+
+
+        # make prediction
+        yolos = yolov3.predict(image)
+        # define the probability threshold for detected object
+        boxes = list()
+
+        for i in range(len(yolos)):
+                # decode the output of the network
+            boxes += decode_netout(yolos[i][0], anchors[i], obj_thresh,  net_h, net_w)
+
+            # correct the sizes of the bounding boxes
+        correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
+
+        # suppress non-maximal boxes
+        do_nms(boxes, nms_thresh)
+
+        # get the details of the detected objects
+        v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+        # summarize what we found
+        j=0
+        for b in v_boxes:
+
+            x,y,w,h=b.xmin,b.ymin,b.xmax,b.ymax
+            cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=2)
+            cv2.putText(test_img, str(v_labels[j])+" "+str(round(v_scores[j],2)), (int(x), int(y)), cv2.LINE_AA, 1, (0,0,255), 2)
+            j+=1
+        #for i in range(len(v_boxes)):
+            #print(v_labels[i], v_scores[i])
+            #cv2.putText(test_img, v_labels[i], (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        # draw what we found
+        #draw_boxes(im_name, v_boxes, v_labels, v_scores)
+        imgencode=cv2.imencode('.jpg',test_img)[1]
+        
+        stringData=imgencode.tostring()
+        
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        
+        #resized_img = cv2.resize(test_img, (1000, 700))
+        #cv2.imshow('Object detection',resized_img)
+        #if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+            #break
+
+    #cap.release()
+    #cv2.destroyAllWindows
+        
+        
+
+    del(cap)
+
+@app.route('/calc')
+def calc():
+     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    #app.run(host='localhost', debug=True, threaded=True)
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 9000, app)
+
